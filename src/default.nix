7a7d110d01812178494f033c55730f92f2b831e6
Robin Bate Boerop <me@robinbb.com>
2020-12-06 14:39:54 -0400

    Build an env that can be prepended to PATH

--- src/default.nix
+++ src/default.nix
@@ -1,15 +1,23 @@
 let
-  config = {
-    checkMeta = true;
-    checkMetaRecursively = true;
-    doCheckByDefault = true;
-  };
+  config =
+    { checkMeta = true;
+      checkMetaRecursively = true;
+      doCheckByDefault = true;
+    };
   nixpkgs =
     import ./nixpkgs config;
+  r10-nix =
+    nixpkgs.nix.override {
+      name = "r10-nix";
+      storeDir = "/opt/nix";
+      stateDir = "/var";
+      confDir = "/nonexistent-nix-conf";
+    };
 in
-  nixpkgs.nix.override {
-    name = "r10-nix";
-    storeDir = "/opt/nix";
-    stateDir = "/var";
-    confDir = "/nonexistent-nix-conf";
+  nixpkgs.buildEnv {
+    name = "r10-env";
+    paths = [
+      r10-nix
+      nixpkgs.cacert
+    ];
   }
