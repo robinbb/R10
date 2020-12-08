db021ec8ef0a110cb55f215df311add625766b52
Robin Bate Boerop <me@robinbb.com>
2020-12-08 15:52:12 -0400

    Build more packages

--- src/default.nix
+++ src/default.nix
@@ -12,6 +12,8 @@ let
     };
   nixpkgs =
     import ./nixpkgs config;
+  broken-nixpkgs =
+    import ./nixpkgs {};
   r10-nix =
     nixpkgs.nix.override {
       name = "r10-nix";
@@ -19,24 +21,57 @@ let
       stateDir = "/var";
       confDir = "/nonexistent-nix-conf";
     };
-in
-  nixpkgs.buildEnv {
-    name = "r10-env";
-    paths = [
-      # Bootstrap R10.
-      r10-nix
-    ] ++ (with nixpkgs; [
-      # Build some crucial packages.
+  crucial-packages =
+    with nixpkgs; [
       bash
       cacert
       dash
       gcc
       gitMinimal
+      gnused
       mosh
+      nix-prefetch-git
       openssh
       stow
       tmux
       tree
       vim
-    ]);
+    ];
+  interesting-packages =
+    with nixpkgs; [
+      awscli
+      gnupg
+      htop
+      ispell
+      lsof
+      lolcat
+      mercurial
+      mysql
+      nix-generate-from-cpan
+      nodejs
+      postgresql
+      pstree
+      ag
+      unzip
+      wget
+      zip
+    ];
+  broken-packages =
+    with broken-nixpkgs; [
+      bazel
+      cabal2nix
+      direnv
+      ghc
+      git-lfs
+      gitAndTools.git-annex
+      pinentry
+      stack
+    ];
+in
+  nixpkgs.buildEnv {
+    name = "r10-env";
+    paths = [ r10-nix ]
+      ++ crucial-packages
+      ++ interesting-packages
+      ;
   }
